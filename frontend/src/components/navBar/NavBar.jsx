159887e6c0ce1c61e3d641ad6011dd1be58686a7
karrimDCI <karim_afettouche@hotmail.com>
2022-06-24 15:24:29 +0200

    responsive design

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,36 +1,56 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 import "./navBar.scss";
 
+const variants = {
+  open: { opacity: 0, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+};
+
 const NavBar = () => {
+  const [show, setShow] = useState(false);
+  const [toggle, setToggle] = useState(true);
   return (
     <header className="header header-bg">
       <nav className="nav collapsible">
-        <a href="/">
-          <img src="" alt="" />
-          KuCa NiNo
-        </a>
-        <div className="btn-burger-menu active">
-          <GiHamburgerMenu className="nav__toggler" color="#fff" />
+        <div className="nav-logo">
+          <a href="/">
+            <img src="" alt="" />
+            KuCa NiNo
+          </a>
         </div>
-        <ul className="list nav__list collapsible__content">
-          <li className="nav__item">
-            <a href="">WOHNEN</a>
-          </li>
-          <li className="nav__item">
-            <a href="">ZIMMER</a>
-          </li>
-          <li className="nav__item">
-            <a href="">GARTEN</a>
-          </li>
-          <li className="nav__item">
-            <a href="">INSEL</a>
-          </li>
-        </ul>
+        <div className="nav-menu-items">
+          <ul className="list nav__list collapsible__content">
+            <li className="nav__item">
+              <a href="">WOHNEN</a>
+            </li>
+            <li className="nav__item">
+              <a href="">ZIMMER</a>
+            </li>
+            <li className="nav__item">
+              <a href="">GARTEN</a>
+            </li>
+            <li className="nav__item">
+              <a href="">INSEL</a>
+            </li>
+          </ul>
+        </div>
         <div className="anfragen-btn">
           <button className="booking--btn">Buchung Anfragen</button>
         </div>
       </nav>
+      <div
+        onClick={() => setToggle(!toggle)}
+        className={
+          toggle ? "toggler nav__toggler" : "toggler nav__toggler active"
+        }
+      >
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
     </header>
   );
 };
