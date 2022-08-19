5a8ac2391add6b32854b93765c7cecd4ff7656f4
Karim Afettouche <krymo86@yahoo.com>
2022-08-19 10:55:45 +0200

    restart from A

--- frontend/src/AnimatedRoutes.jsx
+++ frontend/src/AnimatedRoutes.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import NavBar from "./components/headerPage/NavBar";
 
 function AnimatedRoutes() {
   const location = useLocation();
   return (
     <AnimatePresence>
-      <NavBar />
       <Routes location={location} key={location.pathname}>
         <Route />
       </Routes>
