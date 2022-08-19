2c15d82ca868f100d05fa0fff07f2e3992ff93a0
Karim Afettouche <krymo86@yahoo.com>
2022-08-19 11:01:21 +0200

    navBar + footer + welcomePage initialization

---
+++ frontend/src/components/welcomePage/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./welcomePage.scss";
+
+function WelcomePage() {
+  return (
+    <div>
+      <h1>Hallo world</h1>
+    </div>
+  );
+}
+
+export default WelcomePage;
